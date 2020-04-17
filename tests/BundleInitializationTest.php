378f224da16673a3213f26b81212c213efdc2b2c
Klaus <support@kikwik.it>
2020-04-17 23:29:01 +0200

    test

--- tests/BundleInitializationTest.php
+++ tests/BundleInitializationTest.php
@@ -17,12 +17,12 @@ class BundleInitializationTest extends BaseBundleTestCase
         return KikwikIpTraceableListenerBundle::class;
     }
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
         $this->addCompilerPass(new PublicServicePass('|gedmo.ip_traceable.ip_traceable_listener|'));
-        $this->addCompilerPass(new PublicServicePass('|kikwik.ip_traceable_listener.event_subscriber.ip_trace_subscriber|'));
+        $this->addCompilerPass(new PublicServicePass('|kikwik.ip_traceable_listener.event_subscriber.ip_traceable_subscriber|'));
     }
 
 
@@ -39,8 +39,8 @@ class BundleInitializationTest extends BaseBundleTestCase
         $service = $container->get('gedmo.ip_traceable.ip_traceable_listener');
         $this->assertInstanceOf(IpTraceableListener::class, $service, 'Service gedmo.ip_traceable.ip_traceable_listener is instance of IpTraceableListener');
 
-        $this->assertTrue($container->has('kikwik.ip_traceable_listener.event_subscriber.ip_trace_subscriber'),'Conteiner has kikwik.ip_traceable_listener.event_subscriber.ip_trace_subscriber');
-        $service = $container->get('kikwik.ip_traceable_listener.event_subscriber.ip_trace_subscriber');
-        $this->assertInstanceOf(IpTraceableSubscriber::class, $service,'Service kikwik.ip_traceable_listener.event_subscriber.ip_trace_subscriber is instance of IpTraceSubscriber');
+        $this->assertTrue($container->has('kikwik.ip_traceable_listener.event_subscriber.ip_traceable_subscriber'),'Conteiner has kikwik.ip_traceable_listener.event_subscriber.ip_traceable_subscriber');
+        $service = $container->get('kikwik.ip_traceable_listener.event_subscriber.ip_traceable_subscriber');
+        $this->assertInstanceOf(IpTraceableSubscriber::class, $service,'Service kikwik.ip_traceable_listener.event_subscriber.ip_traceable_subscriber is instance of IpTraceSubscriber');
     }
 }
